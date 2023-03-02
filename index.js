c3ff3b4ec2e25ab4e3353ded0e6f40ca7b63aad0
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-03-01 21:10:19 -0300

    Instanceof 3

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 "use strict";
-class Product {
-    constructor(nome) {
-        this.nome = nome;
-    }
-}
-const livro = new Product("A Guerra Dos Tronos");
-console.log(livro.nome);
--- index.ts
+++ index.ts
@@ -1,12 +1,2 @@
-class Product{
-    
-    nome:string;
-    constructor(nome:string){
-        this.nome = nome;
-    }
-    
-}
 
-const livro = new Product("A Guerra Dos Tronos");
 
-console.log(livro.nome);
