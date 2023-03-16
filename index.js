78190c15a5d4ddf71161e86e2d971df81f53c8cb
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-03-16 16:43:46 -0300

    Generics

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 "use strict";
+/* function retorno<T>(a:T):T{
+    if( typeof a === "string") return a;
+
+    return a;
+}
+
+console.log(retorno("A Game")); */
+var _a, _b;
+/* const numeros = [0,1,2,3,4,5,6,7,8,9];
+const frutas = ["banana","Maçã","Goiaba","Pera","Uva","Abacaxi","Mamão"];
+
+function firstFive<T>(lista:T[]){
+    return lista.slice(0,5);
+}
+
+console.log(firstFive(frutas)); */
+function notNull(a) {
+    if (a !== null)
+        return a;
+    return null;
+}
+(_a = notNull("Teste")) === null || _a === void 0 ? void 0 : _a.length;
+(_b = notNull(33)) === null || _b === void 0 ? void 0 : _b.toFixed();
--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
 
+/* function retorno<T>(a:T):T{
+    if( typeof a === "string") return a;
+
+    return a;
+}
+
+console.log(retorno("A Game")); */
+
+/* const numeros = [0,1,2,3,4,5,6,7,8,9];
+const frutas = ["banana","Maçã","Goiaba","Pera","Uva","Abacaxi","Mamão"];
+
+function firstFive<T>(lista:T[]){
+    return lista.slice(0,5);
+}
+
+console.log(firstFive(frutas)); */
+
+function notNull<T>(a:T){
+    if(a !== null) return a;
+    return null;
+}
+
+notNull("Teste")?.length;
+notNull(33)?.toFixed();
