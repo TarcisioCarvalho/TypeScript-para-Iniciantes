0dbaa84f0029649f16d7a205f295c093f9ea450b
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-04-06 21:18:57 -0300

    Type Guard

--- index.js
+++ index.js
@@ -1,73 +1,26 @@
 "use strict";
-function Arredonda(value) {
-    switch (typeof value) {
-        case "number":
-            console.log(typeof value);
-            return Math.ceil(value);
-        case "string":
-            console.log(typeof value);
-            return Math.ceil(Number(value)).toString();
-        default:
-            break;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function fecthProduto() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch("https://api.origamid.dev/json/notebook.json");
+        const json = yield response.json();
+        handleProduto(json);
+    });
+}
+function handleProduto(data) {
+    console.log(data);
+    if ("preco" in data) {
+        document.body.innerHTML += `
+    <p>Nome:${data.nome}</p>
+    <p>Preço:${data.preco}</p>
+    `;
     }
 }
-console.log(Arredonda(3.6));
-console.log(Arredonda("3.4"));
-/* function retorno<T>(a:T):T{
-    if( typeof a === "string") return a;
-
-    return a;
-}
-
-console.log(retorno("A Game")); */
-/* const numeros = [0,1,2,3,4,5,6,7,8,9];
-const frutas = ["banana","Maçã","Goiaba","Pera","Uva","Abacaxi","Mamão"];
-
-function firstFive<T>(lista:T[]){
-    return lista.slice(0,5);
-}
-
-console.log(firstFive(frutas)); */
-// function notNull<T>(a:T){
-//     if(a !== null) return a;
-//     return null;
-// }
-// notNull("Teste")?.length;
-// notNull(33)?.toFixed();
-/* interface Forma{
-    lado:number;
-    perimetro(lado:number) : number
-}
-
-
-function Calcula(forma:Forma){
-    console.log(forma.perimetro(4))
-}
-
-Calcula({
-    lado:10,
-    perimetro(lado) {
-        return lado*4
-    },
-});
-
-function $(seletor:'a'):HTMLAnchorElement | null
-function $(seletor:"video"):HTMLVideoElement | null
-function $(seletor:"audio"): HTMLAudioElement | null
-function $(seletor:"img"): HTMLImageElement | null
-function $(seletor:"button"): HTMLButtonElement | null
-function $(seletor:"form"): HTMLFormElement | null
-function $(seletor:"div"):HTMLDivElement | null
-function $(seletor:"input"):HTMLInputElement | null
-function $(seletor:string):Element | null{
-    return document.querySelector(seletor);
-}
-
-console.log($("a"));
-console.log($("video"));
-console.log($("audio")?.volume);
-console.log($("img"));
-console.log($("button"));
-console.log($("form"));
-console.log($("div"));
-console.log($("input")); */ 
--- index.ts
+++ index.ts
@@ -1,80 +1,19 @@
-function Arredonda(value:string):string
-function Arredonda(value:number):number
-function Arredonda(value:number | string):number | string | undefined{
-    switch (typeof value) {
-        case "number":
-            console.log(typeof value);
-            return Math.ceil(value);
-        case "string":
-            console.log(typeof value);
-            return Math.ceil(Number(value)).toString();
-        default:
-            break;
+async function  fecthProduto(){
+    const response = await fetch("https://api.origamid.dev/json/notebook.json");
+    const json = await response.json();
+    handleProduto(json);
+}
+
+interface Prduto{
+    nome:string,
+    preco:number
+}
+function handleProduto(data:Prduto){
+    console.log(data);
+    if("preco" in data){
+        document.body.innerHTML+=`
+    <p>Nome:${data.nome}</p>
+    <p>Preço:${data.preco}</p>
+    `;
     }
 }
-console.log(Arredonda(3.6));
-console.log(Arredonda("3.4"));
-
-
-/* function retorno<T>(a:T):T{
-    if( typeof a === "string") return a;
-
-    return a;
-}
-
-console.log(retorno("A Game")); */
-
-/* const numeros = [0,1,2,3,4,5,6,7,8,9];
-const frutas = ["banana","Maçã","Goiaba","Pera","Uva","Abacaxi","Mamão"];
-
-function firstFive<T>(lista:T[]){
-    return lista.slice(0,5);
-}
-
-console.log(firstFive(frutas)); */
-
-// function notNull<T>(a:T){
-//     if(a !== null) return a;
-//     return null;
-// }
-
-// notNull("Teste")?.length;
-// notNull(33)?.toFixed();
-
-/* interface Forma{
-    lado:number;
-    perimetro(lado:number) : number
-}
-
-
-function Calcula(forma:Forma){
-    console.log(forma.perimetro(4))
-}
-
-Calcula({
-    lado:10,
-    perimetro(lado) {
-        return lado*4
-    },
-});
-
-function $(seletor:'a'):HTMLAnchorElement | null
-function $(seletor:"video"):HTMLVideoElement | null
-function $(seletor:"audio"): HTMLAudioElement | null
-function $(seletor:"img"): HTMLImageElement | null
-function $(seletor:"button"): HTMLButtonElement | null
-function $(seletor:"form"): HTMLFormElement | null
-function $(seletor:"div"):HTMLDivElement | null
-function $(seletor:"input"):HTMLInputElement | null
-function $(seletor:string):Element | null{
-    return document.querySelector(seletor);
-}
-
-console.log($("a"));
-console.log($("video"));
-console.log($("audio")?.volume);
-console.log($("img"));
-console.log($("button"));
-console.log($("form"));
-console.log($("div"));
-console.log($("input")); */
